66f2fd1a45acf8e9ec2da0a1654d70c2a42d8b8d
r4ficore <100816536+r4ficore@users.noreply.github.com>
2025-04-26 23:03:16 +0200

    Update checkPulse.js

--- api/checkPulse.js
+++ api/checkPulse.js
@@ -1,20 +1,18 @@
 // /api/checkPulse.js
 
-import { lastPulse } from './sendPulse';
-
 export default function handler(req, res) {
   if (req.method === 'GET') {
+    const { lastPulse } = require('./sendPulse'); // Dynamiczne wczytanie za każdym razem
+
     if (lastPulse) {
       res.status(200).json({
         found: true,
         pulse: lastPulse
       });
     } else {
-      res.status(200).json({
-        found: false
-      });
+      res.status(200).json({ found: false });
     }
   } else {
-    res.status(405).json({ error: "Method not allowed" });
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
